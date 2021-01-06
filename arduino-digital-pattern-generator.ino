0964b8cdab9e1147a65dc1f7129fbc732f113018
francesco-scar <francesco_git@protonmail.com>
2021-01-06 22:31:55 +0100

    :sparkles: Clock function (commented)

--- arduino-digital-pattern-generator.ino
+++ arduino-digital-pattern-generator.ino
@@ -1,8 +1,8 @@
-void clockGenerator();
-void digitalPatternGenerator();
-
 void setup() {
   Serial.begin(9600);
+
+  pinMode(2, OUTPUT);
+  digitalWrite(2, LOW);
 }
 
 void loop() {
@@ -73,6 +73,64 @@ void clockGenerator(){
     goto clockFunctionBegin;
   }
 }
+
+/*
+void digitalPatternGenerator(){
+  patternFunctionBegin:
+  Serial.print("Frequency [Hz]:  ");
+  float freq = inputNumber();
+  Serial.println("Pattern string (256 byte max, truncated if more) in binary (introduced by B) or hexadecimal (introduced by X) [eg. B101110 or X2e]");
+  uint8_t patern[64];
+  uint8_t patternLength; 
+//  getPattern(&patternLength, pattern);
+  
+  if (cycles){
+    Serial.print("Generating ");
+    Serial.print(cycles);
+    Serial.print(" clock pulses at ");
+    Serial.print(freq);   // TODO: format frequency as kHz, MHz...
+    Serial.println(" Hz on pin 2 ...");
+  } else {
+    Serial.print("Generating infinite clock pulses at ");
+    Serial.print(freq);
+    Serial.println(" Hz on pin 2 ...");
+  }
+
+  unsigned long generatedPulses = 0;
+  bool pinState = false;
+  pinMode(2, OUTPUT);
+  digitalWrite(2, LOW);
+  unsigned long beginTime = millis();
+
+  while (generatedPulses < cycles || cycles == 0){
+    if (millis() >= (generatedPulses/freq + pinState/(2*freq))*1000 + beginTime){
+      pinState = !pinState;
+      digitalWrite(2, pinState);
+
+      if (pinState == 0){
+        generatedPulses++;          // Temporary ignoring long overflow (at least after 1000 sec ~2:45 h)
+      }
+    }
+  }
+
+  delay(50);
+
+  serialFlush();
+  
+  Serial.println("Done!\n\nEnter 1 to select clock option again other to return to main menu");
+  while (!Serial.available());
+  if (Serial.read() == '1'){
+    goto patternFunctionBegin;
+  }
+}
+
+
+
+
+void getPattern(uint8_t ){
+  
+}
+*/
 
 float inputNumber(){
   float n = 0;
